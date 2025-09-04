cf4e2969575f06b1270ec6567f7497a6c699a648
ElMo0977 <molocura@gmail.com>
2025-09-04 15:11:16 +0200

    Fix App import path

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './new_App';
 
 // Punto de entrada de la aplicación
 const rootElement = document.getElementById('root') as HTMLElement;
